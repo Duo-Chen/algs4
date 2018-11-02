4a953e2ac3d4db48bd4bad1aef91f96c455bfdd1
Guan_Liang_Chen <Guan_Liang_Chen@Dell.com>
2018-11-02 15:54:23 +0800

    add BoggleSolver test

--- Boggle/src/BoggleSolver.java
+++ Boggle/src/BoggleSolver.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class BoggleSolver {
     public BoggleSolver(String[] dictionary) {
         throw new IllegalArgumentException("not implemented");
@@ -9,5 +12,18 @@ public class BoggleSolver {
 
     public int scoreOf(String word) {
         throw new IllegalArgumentException("not implemented");
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] dict = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dict);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
     }
 }
--- Boggle/src/Main.java
+++ Boggle/src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String[][] strings = {
+                { "dictionary-algs4.txt", "board4x4.txt" },
+                { "dictionary-algs4.txt", "board-q.txt" }
+        };
+
+        for (String[] str : strings)
+            BoggleSolver.main(str);
     }
 }
