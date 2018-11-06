610e8c5498f3e8b79aae631142ae66811b63344b
Guan_Liang_Chen <Guan_Liang_Chen@Dell.com>
2018-11-06 14:17:36 +0800

    Use trie set for performance

--- Boggle/src/BoggleSolver.java
+++ Boggle/src/BoggleSolver.java
@@ -1,4 +1,3 @@
-import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -6,167 +5,82 @@ import java.util.Arrays;
 
 
 public class BoggleSolver {
-    private static class PrefixTST<Value> {
-        private int n;              // size
-        private Node<Value> root;   // root of TST
+    private final PrefixTrieSET dict;
+    private BoggleBoard board;
+    private SET<String> validStrings;
 
-        private static class Node<Value> {
-            private char c;                        // character
-            private Node<Value> left, mid, right;  // left, middle, and right subtries
-            private Value val;                     // value associated with string
+    private static class PrefixTrieSET {
+        private static final int R = 26;        // A - Z
+        private static final int A = 65;
+
+        private Node root;      // root of trie
+
+        // R-way trie node
+        private static class Node {
+            private Node[] next = new Node[R];
+            private boolean isString;
         }
 
-        public PrefixTST() {
-        }
-
-        public int size() {
-            return n;
+        public PrefixTrieSET() {
         }
 
         public boolean contains(String key) {
-            if (key == null) {
-                throw new IllegalArgumentException("argument to contains() is null");
-            }
-            return get(key) != null;
+            if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+            Node x = get(root, key, 0);
+            if (x == null) return false;
+            return x.isString;
         }
 
-        public Value get(String key) {
-            if (key == null) {
-                throw new IllegalArgumentException("calls get() with null argument");
-            }
-            if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
-            Node<Value> x = get(root, key, 0);
+        private Node get(Node x, String key, int d) {
             if (x == null) return null;
-            return x.val;
-        }
-
-        // return subtrie corresponding to given key
-        private Node<Value> get(Node<Value> x, String key, int d) {
-            if (x == null) return null;
-            if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+            if (d == key.length()) return x;
             char c = key.charAt(d);
-            if      (c < x.c)              return get(x.left,  key, d);
-            else if (c > x.c)              return get(x.right, key, d);
-            else if (d < key.length() - 1) return get(x.mid,   key, d+1);
-            else                           return x;
+            int index = c - A;
+            return get(x.next[index], key, d+1);
         }
 
-        public void put(String key, Value val) {
-            if (key == null) {
-                throw new IllegalArgumentException("calls put() with null key");
-            }
-            if (!contains(key)) n++;
-            root = put(root, key, val, 0);
+        public void add(String key) {
+            if (key == null) throw new IllegalArgumentException("argument to add() is null");
+            root = add(root, key, 0);
         }
 
-        private Node<Value> put(Node<Value> x, String key, Value val, int d) {
-            char c = key.charAt(d);
-            if (x == null) {
-                x = new Node<Value>();
-                x.c = c;
+        private Node add(Node x, String key, int d) {
+            if (x == null) x = new Node();
+            if (d == key.length()) {
+                x.isString = true;
             }
-            if      (c < x.c)               x.left  = put(x.left,  key, val, d);
-            else if (c > x.c)               x.right = put(x.right, key, val, d);
-            else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
-            else                            x.val   = val;
+            else {
+                char c = key.charAt(d);
+                int index = c - A;
+                x.next[index] = add(x.next[index], key, d+1);
+            }
             return x;
         }
 
-        public String longestPrefixOf(String query) {
-            if (query == null) {
-                throw new IllegalArgumentException("calls longestPrefixOf() with null argument");
-            }
-            if (query.length() == 0) return null;
-            int length = 0;
-            Node<Value> x = root;
-            int i = 0;
-            while (x != null && i < query.length()) {
-                char c = query.charAt(i);
-                if      (c < x.c) x = x.left;
-                else if (c > x.c) x = x.right;
-                else {
-                    i++;
-                    if (x.val != null) length = i;
-                    x = x.mid;
-                }
-            }
-            return query.substring(0, length);
-        }
-
-        public Iterable<String> keys() {
-            Queue<String> queue = new Queue<String>();
-            collect(root, new StringBuilder(), queue);
-            return queue;
-        }
-
-        public Iterable<String> keysWithPrefix(String prefix) {
-            if (prefix == null) {
-                throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
-            }
-            Queue<String> queue = new Queue<String>();
-            Node<Value> x = get(root, prefix, 0);
-            if (x == null) return queue;
-            if (x.val != null) queue.enqueue(prefix);
-            collect(x.mid, new StringBuilder(prefix), queue);
-            return queue;
-        }
-
-        // all keys in subtrie rooted at x with given prefix
-        private void collect(Node<Value> x, StringBuilder prefix, Queue<String> queue) {
-            if (x == null) return;
-            collect(x.left,  prefix, queue);
-            if (x.val != null) queue.enqueue(prefix.toString() + x.c);
-            collect(x.mid,   prefix.append(x.c), queue);
-            prefix.deleteCharAt(prefix.length() - 1);
-            collect(x.right, prefix, queue);
-        }
-
-        public Iterable<String> keysThatMatch(String pattern) {
-            Queue<String> queue = new Queue<String>();
-            collect(root, new StringBuilder(), 0, pattern, queue);
-            return queue;
-        }
-
-        private void collect(Node<Value> x, StringBuilder prefix, int i, String pattern, Queue<String> queue) {
-            if (x == null) return;
-            char c = pattern.charAt(i);
-            if (c == '.' || c < x.c) collect(x.left, prefix, i, pattern, queue);
-            if (c == '.' || c == x.c) {
-                if (i == pattern.length() - 1 && x.val != null) queue.enqueue(prefix.toString() + x.c);
-                if (i < pattern.length() - 1) {
-                    collect(x.mid, prefix.append(x.c), i+1, pattern, queue);
-                    prefix.deleteCharAt(prefix.length() - 1);
-                }
-            }
-            if (c == '.' || c > x.c) collect(x.right, prefix, i, pattern, queue);
-        }
-
         public boolean hasPrefix(String prefix) {
-            Node<Value> prefixNode = get(root, prefix, 0);
+            Node prefixNode = get(root, prefix, 0);
             if (prefixNode == null)
                 return false;
-            if (prefixNode.val != null)
+            if (prefixNode.isString)
                 return true;
-            if (prefixNode.left == null && prefixNode.mid == null && prefixNode.right == null)
-                return false;
-            return true;
+            for (int i = 0; i < R; i++)
+                if (prefixNode.next[i] != null)
+                    return true;
+
+            return false;
         }
     }
-
-    private final PrefixTST<Integer> dict;
-    private BoggleBoard board;
-    private SET<String> validStrings;
 
     public BoggleSolver(String[] dictionary) {
         if (dictionary == null)
             throw new IllegalArgumentException("");
 
-        dict = new PrefixTST<>();
+        dict = new PrefixTrieSET();
         for (String str : dictionary) {
             if (str.length() < 3)
                 continue;
 
-            dict.put(str, length2point(str.length()));
+            dict.add(str);
         }
     }
 
@@ -182,7 +96,7 @@ public class BoggleSolver {
         for (int x = 0; x < col; x++) {
             for (int y  = 0; y < row; y++) {
                 boolean[][] marked = new boolean[col][row];
-                findValidString(marked, x, y, new String());
+                findValidString(marked, x, y, "");
             }
         }
 
@@ -259,7 +173,7 @@ public class BoggleSolver {
             return 0;
 
         if (dict.contains(word))
-            return dict.get(word);
+            return length2point(word.length());
 
         return 0;
     }
