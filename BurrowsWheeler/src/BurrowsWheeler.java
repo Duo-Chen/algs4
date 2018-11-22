f08da32abd3fb254987810021723ed0590a5b276
Guan_Liang_Chen <Guan_Liang_Chen@Dell.com>
2018-11-22 15:54:18 +0800

    init commit

---
+++ BurrowsWheeler/src/BurrowsWheeler.java
@@ -0,0 +1,17 @@
+public class BurrowsWheeler {
+    // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
+    public static void transform() {
+        throw new IllegalArgumentException("not implemented");
+    }
+
+    // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
+    public static void inverseTransform() {
+        throw new IllegalArgumentException("not implemented");
+    }
+
+    // if args[0] is '-', apply Burrows-Wheeler transform
+    // if args[0] is '+', apply Burrows-Wheeler inverse transform
+    public static void main(String[] args) {
+        throw new IllegalArgumentException("not implemented");
+    }
+}
---
+++ BurrowsWheeler/src/CircularSuffixArray.java
@@ -0,0 +1,21 @@
+public class CircularSuffixArray {
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+        throw new IllegalArgumentException("not implemented");
+    }
+
+    // length of s
+    public int length() {
+        throw new IllegalArgumentException("not implemented");
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        throw new IllegalArgumentException("not implemented");
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        throw new IllegalArgumentException("not implemented");
+    }
+}
