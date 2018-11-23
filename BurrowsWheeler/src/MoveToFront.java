8658160b2431901577590d1057aebe4d0afd79e7
Guan_Liang_Chen <Guan_Liang_Chen@Dell.com>
2018-11-23 13:07:06 +0800

    MoveToFront class

--- BurrowsWheeler/src/MoveToFront.java
+++ BurrowsWheeler/src/MoveToFront.java
@@ -1,12 +1,72 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class MoveToFront {
+    private static final int ASCII_SIZE = 256;
+
+    private static int[] createCodeMap() {
+        int[] res = new int[ASCII_SIZE];
+        for (int i = 0; i < ASCII_SIZE; i++)
+            res[i] = i;
+
+        return res;
+    }
+
+    private static int findIndex(int[] codeMap, int num, int c) {
+        int index = c;
+        if (codeMap[index] == c)
+            return index;
+
+        for (int i = 0; i < num; i++)
+            if (codeMap[i] == c)
+                return i;
+
+        for (int i = Math.max(num, index - num); i < Math.min(ASCII_SIZE, index + num); i++)
+            if (codeMap[i] == c)
+                return i;
+
+        return -1;
+    }
+
+    private static void shift(int[] codeMap, int index) {
+        int temp = codeMap[index];
+        for (int i = index; i > 0; i--)
+            codeMap[i] = codeMap[i - 1];
+
+        codeMap[0] = temp;
+    }
+
+    private static int move2Front(int[] codeMap, int num, int c) {
+        // find index
+        int index = findIndex(codeMap, num, c);
+
+        // shift
+        shift(codeMap, index);
+
+        return index;
+    }
+
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
-        throw new IllegalArgumentException("not implemented");
+        int[] codeMap = createCodeMap();
+        int num = 0;
+
+        while (!BinaryStdIn.isEmpty()) {
+            char c = BinaryStdIn.readChar();
+            StdOut.print(move2Front(codeMap, num, c) + " ");
+            num++;
+        }
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode() {
-        throw new IllegalArgumentException("not implemented");
+        int[] codeMap = createCodeMap();
+
+        while (!BinaryStdIn.isEmpty()) {
+            char c = BinaryStdIn.readChar();
+            StdOut.print((char)codeMap[c]);
+            shift(codeMap, c);
+        }
     }
 
     // if args[0] is '-', apply move-to-front encoding
