6a16b84d77d6ddf107274d70ef7d419dfbc9712a
Guan_Liang_Chen <Guan_Liang_Chen@Dell.com>
2018-11-23 13:29:10 +0800

    CircularSuffixArray class

--- BurrowsWheeler/src/CircularSuffixArray.java
+++ BurrowsWheeler/src/CircularSuffixArray.java
@@ -1,21 +1,48 @@
+import java.util.TreeMap;
+
 public class CircularSuffixArray {
+    private final int len;
+    private final int[] indices;
+
     // circular suffix array of s
     public CircularSuffixArray(String s) {
-        throw new IllegalArgumentException("not implemented");
+        if (s == null)
+            throw new IllegalArgumentException("");
+
+        len = s.length();
+        indices = new int[len];
+        TreeMap<String, Integer> set = new TreeMap<>();
+        for (int i = 0; i < len; i++) {
+            String buff = s.substring(i) + s.substring(0, i);
+            set.put(buff, i);
+        }
+
+        int index = 0;
+        for (String key : set.keySet()) {
+            indices[index++] = set.get(key);
+        }
     }
 
     // length of s
     public int length() {
-        throw new IllegalArgumentException("not implemented");
+        return len;
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
-        throw new IllegalArgumentException("not implemented");
+        if (i < 0 || i >= len)
+            throw new IllegalArgumentException("out of range");
+
+        return indices[i];
     }
 
     // unit testing (required)
     public static void main(String[] args) {
-        throw new IllegalArgumentException("not implemented");
+        CircularSuffixArray csa = new CircularSuffixArray("ABRACADABRA!");
+        int[] ans = { 11, 10, 7, 0, 3, 5, 8, 1, 4, 6, 9, 2 };
+        for (int i = 0; i < ans.length; i++) {
+            if (ans[i] != csa.index(i))
+                throw new IllegalArgumentException("wrong answer");
+        }
     }
 }
