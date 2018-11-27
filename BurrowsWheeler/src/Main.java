dd3c9c2ea74368234bc624f7dfa0261787c2e425
Guan_Liang_Chen <Guan_Liang_Chen@Dell.com>
2018-11-27 14:21:04 +0800

    add timing test

--- BurrowsWheeler/src/Main.java
+++ BurrowsWheeler/src/Main.java
@@ -1,6 +1,28 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        In in = new In("amendments.txt");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 16000; i++)
+            sb.append(in.readChar());
+
+        long start = System.currentTimeMillis();
+        int count = 0;
+        while (true) {
+            long t = System.currentTimeMillis();
+            if (t > start + 10000)
+                break;
+            CircularSuffixArray csa = new CircularSuffixArray(sb.toString());
+            for (int i = 0; i < csa.length(); i++) ;
+
+            count++;
+        }
+
+        StdOut.println(count);
     }
 }
